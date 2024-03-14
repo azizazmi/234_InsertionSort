211ec48be3b09cc09d3b53781d308e8b1d4f058b
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-03-14 15:28:26 +0700

    memperbaiki beberapa bagian

--- ASD_Pert4_234_InsertionSort.cpp
+++ ASD_Pert4_234_InsertionSort.cpp
@@ -29,11 +29,11 @@ void insertionSort() {
     int i, j, temp;
 
     for(i = 1; i <= n - 1; i++) {   //step 1
-        temp = arr[1];  //step 2
+        temp = arr[i];  //step 2
 
         j = i - 1;  //step 3
 
-        while (j <= 0 && arr[j] > temp)   //step 4
+        while (j >= 0 && arr[j] > temp)   //step 4
         {
             arr[j + 1] = arr[j]; //step 4a
             j--;  //step 4b
