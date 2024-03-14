4c0f24a114aabe17bfa3bb07bd9ba47445148097
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-03-14 14:27:55 +0700

    Add project files.

---
+++ ASD_Pert4_234_InsertionSort.cpp
@@ -0,0 +1,9 @@
+// ASD_Pert4_234_InsertionSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
